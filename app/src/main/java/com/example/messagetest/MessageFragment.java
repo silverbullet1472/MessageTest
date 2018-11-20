d9ddd3f9f2019f48825f5d0f9935ae9d3cc547a9
ricky <mrdingjiaqi@gmail.com>
2018-11-20 11:45:36 +0800

    typo fixed

--- app/src/main/java/com/example/messagetest/MessageFragment.java
+++ app/src/main/java/com/example/messagetest/MessageFragment.java
@@ -27,7 +27,7 @@ public class MessageFragment extends Fragment {
     private List<MyMessage> messages;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.message_fragment,container,false);
+        View view = inflater.inflate(R.layout.fragment_message,container,false);
         initView(view);
         return view;
     }
